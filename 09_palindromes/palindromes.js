bc483ce6be14c83720569b5f76271d414941bf61
Eximo <eximo888@gmail.com>
2023-03-27 04:51:09 +0200

    solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newString = str.toLowerCase().replace(/[^A-Z0-9]+/ig, "")
+    // console.log(newString);
+    let halfSize = Math.floor(newString.length)
+    if (newString[0] != newString.slice(-1)) return false;
+    for(i = 1; i< halfSize; i++) {
+        if(newString[i] != newString.slice((i*-1) -1,(i*-1))) {
+            // console.log(newString[i]);
+            // console.log(newString.slice((i*-1) -1,(i*-1)));
+            // console.log(`fail i${i}`);
+            return false
+        }
+    }
+    return true;
 };
-
+palindromes("Racecar!");
 // Do not edit below this line
 module.exports = palindromes;
