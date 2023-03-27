e30311348640a6bf72132576c4f2816bb485fa26
Eximo <eximo888@gmail.com>
2023-03-27 05:10:06 +0200

    solved 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+  if (x < 0) return `OOPS`;
+  x = +x;
+  if (x <= 2) return 1;
+  let fibA = 1;
+  let fibB = 1;
+  let fib = 0;
+  for (let i = 2; i < x; i++) {
+    fib = fibA + fibB;
+    fibA = fibB;
+    fibB = fib;
+  }
+  return fib;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
